73317c3354130d1d253100fe922bfeee59f98afa
Travis Caro <travisbcaro@gmail.com>
2017-04-01 19:47:45 -0400

    Add shuttle selector and submission to activiate service

--- src/main/kotlin/com/polaris/app/driver/controller/MenuController.kt
+++ src/main/kotlin/com/polaris/app/driver/controller/MenuController.kt
@@ -5,6 +5,7 @@ import com.polaris.app.driver.service.AuthenticationService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
 import javax.servlet.http.HttpServletRequest
 
 @Controller
@@ -13,11 +14,14 @@ class MenuController(private val authService: AuthenticationService) {
     @RequestMapping("/menu")
     fun menu(model: Model, http: HttpServletRequest) : String {
         if (authService.isAuthenticated(http)) {
-            val userContext = authService.getUserContext(http)
-            model.addAttribute("username", userContext.username)
+            var shuttleOptions: MutableMap<Int, String> = HashMap()
+
+            shuttleOptions.put(1, "Shuttle 1A")
+            shuttleOptions.put(2, "Shuttle 2B")
+            shuttleOptions.put(3, "Shuttle 3C")
+
+            model.addAttribute("shuttleOptions", shuttleOptions)
             return "menu"
-        } else {
-            throw AuthenticationException("Error: user logged out")
-        }
+        } else throw AuthenticationException("Error: user logged out")
     }
 }
--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -5,10 +5,39 @@ function MenuApp() {
 
   self.initialize = function() {
     // TBC : Setup elements
+    elements.shuttleSelect = $('#shuttle-select');
+    elements.btnBeginService = $('#btn-begin-service');
+    elements.activationForm = $('#activation-form');
+
+    elements.btnBeginService.prop('disabled', true);
+    bindEventHandlers();
   };
 
   var bindEventHandlers = function() {
+    elements.shuttleSelect.on('change', function() {
+      ($(this).val() == 0) ?
+          elements.btnBeginService.prop('disabled', true) :
+          elements.btnBeginService.prop('disabled', false);
+    });
 
+    elements.btnBeginService.on('click', function() {
+      if (elements.shuttleSelect.val() != 0) {
+        var form = elements.activationForm;
+        form.append(getInput('selectedShuttleId', elements.shuttleSelect.val()));
+        form.submit();
+      } else {
+        alert('Please select shuttle.');
+      }
+    });
+  };
+
+  var getInput = function(name, value) {
+    var input = $('<input>');
+    input.prop('type', 'hidden');
+    input.prop('name', name);
+    input.val(value);
+
+    return input;
   };
 
   self.initialize();
